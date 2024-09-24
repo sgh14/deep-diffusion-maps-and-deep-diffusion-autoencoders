f7108b4d1c9fe8451b45398187fb87c9f534e349
Sergio García Heredia <sergio.gh14@gmail.com>
2024-09-24 18:36:08 +0200

    Extend changes from swiss_roll and phoneme

--- experiments/mnist/experiment_DDAE.py
+++ experiments/mnist/experiment_DDAE.py
@@ -1,3 +1,6 @@
+import time
+import numpy as np
+import tensorflow as tf
 from os import path
 
 from DeepDiffusionAE import DeepDiffusionAE
@@ -5,17 +8,12 @@ from aux_functions import get_sigma
 from experiments.mnist.plot_results import plot_original, plot_projection, plot_interpolations, plot_history
 from experiments.mnist.load_data import get_datasets
 from experiments.mnist.metrics import compute_metrics
-from experiments.utils import build_encoder, build_decoder
+from experiments.utils import build_conv_encoder, build_conv_decoder
 
 
+seed = 123
+tf.random.set_seed(seed)
 root = 'experiments/mnist/results/DDAE'
-
-diffusion_weights = [0.1, 0.5, 0.9]
-q_vals = [0.005, 0.005, 0.005, 0.005]
-steps_vals = [100, 100, 100, 100]
-alpha_vals = [1, 1, 1, 1]
-kernel = 'rbf'
-
 titles = [
     'Few samples without noise',
     'Many samples without noise',
@@ -23,65 +21,96 @@ titles = [
     'Many samples with noise'
 ]
 
-datasets_train, datasets_test = get_datasets(npoints=10000, test_size=0.1, seed=123, noise=0.25)
+datasets_train, datasets_test = get_datasets(npoints=1000, test_size=0.1, seed=seed, noise=0.25)
 
-for (X, y), title in zip(datasets_train, titles):
+diffusion_weights = [0.975, 0.975, 0.975, 0.975] # [0.8, 0.95] # [0.1, 0.3, 0.5, 0.7, 0.9]
+q_vals = [5e-3, 5e-3, 2.5e-3, 2.5e-3]
+steps_vals = [100, 100, 100, 100]
+alpha_vals = [1, 1, 1, 1]
+kernel = 'rbf'
+
+for i in range(len(titles)):
+    q, steps, alpha, diffusion_weight = q_vals[i], steps_vals[i], alpha_vals[i], diffusion_weights[i]
+    experiment = f'percentile_{q}-steps_{steps}-alpha_{alpha}-diffusion_weight_{diffusion_weight}'
+    X_train, y_train = datasets_train[i]
+    X_test, y_test = datasets_test[i]
+    # Añadimos la dimensión de canal
+    X_train = np.expand_dims(X_train, axis=-1)
+    X_test = np.expand_dims(X_test, axis=-1)
+    title = titles[i]
+    sigma = get_sigma(X_train.reshape((X_train.shape[0], -1)), q)
+    
+    print(experiment, '-', title)  
+    encoder = build_conv_encoder(input_shape=X_train.shape[1:], filters=8, n_components=2, zero_padding=(2, 2))
+    decoder = build_conv_decoder(output_shape=X_train.shape[1:], filters=8, n_components=2, cropping=(2, 2))
+    autoencoder = DeepDiffusionAE(encoder, decoder)
+    tic = time.perf_counter()
+    autoencoder.compile(X_train.reshape((X_train.shape[0], -1)), sigma=sigma, steps=steps, kernel=kernel, alpha=alpha, diffusion_weight=diffusion_weight, optimizer='adam')
+    history = autoencoder.fit(X_train, epochs=10, validation_split=0.1, shuffle=True, batch_size=64, verbose=0)
+
+    X_train_red = autoencoder.encode(X_train)
+    tac = time.perf_counter()
+    X_test_red = autoencoder.encode(X_test)
+    toc = time.perf_counter()
+    X_train_rec = autoencoder.decode(X_train_red).numpy()
+    X_test_rec = autoencoder.decode(X_test_red).numpy()
+
+    time_in_sample = tac - tic
+    time_out_of_sample = toc - tac
+    
     plot_original(
-        X, y, title, path.join(root, 'train_orig'), images_per_class=2, grid_shape=(3, 4)
+        X_train, y_train,
+        path.join(root, title), 'train_orig',
+        images_per_class=2, grid_shape=(3, 4)
     )
-
-for (X, y), title in zip(datasets_test, titles):
     plot_original(
-        X, y, title, path.join(root, 'test_orig'), images_per_class=2, grid_shape=(3, 4)
+        X_test, y_test,
+        path.join(root, title), 'test_orig',
+        images_per_class=2, grid_shape=(3, 4)
     )
+    plot_projection(X_train_red, y_train, path.join(root, title), 'train_red',)
+    plot_original(
+        X_train_rec, y_train,
+        path.join(root, title), 'train_rec',
+        images_per_class=2, grid_shape=(3, 4)
+    )
+    plot_projection(X_test_red, y_test, path.join(root, title), 'test_red',)
+    plot_original(
+        X_test_rec, y_test,
+        path.join(root, title), 'test_rec',
+        images_per_class=2, grid_shape=(3, 4)
+    )
+    plot_interpolations(
+        X_train_red,
+        y_train,
+        autoencoder.decoder,
+        path.join(root, title),
+        'train_interp',
+        class_pairs = [(i, i+1) for i in range(0, 5)],
+        n_interpolations=6
+    )
+    plot_interpolations(
+        X_test_red,
+        y_test,
+        autoencoder.decoder,
+        path.join(root, title),
+        'test_interp',
+        class_pairs = [(i, i+1) for i in range(0, 5)],
+        n_interpolations=6
+    )
+    plot_history(history, path.join(root, 'histories', title), log_scale=True)
 
-for diffusion_weight in diffusion_weights:
-    experiment = f'diffusion_weight_{diffusion_weight}'
-    for i in range(len(titles)):
-        q, steps, alpha = q_vals[i], steps_vals[i], alpha_vals[i]
-        X_train, y_train = datasets_train[i]
-        X_test, y_test = datasets_test[i]
-        title = titles[i]
-
-        img_shape = X_train.shape[1:]
-        X_train = X_train.reshape((X_train.shape[0], -1))
-        X_test = X_test.reshape((X_test.shape[0], -1))
-        
-        sigma = get_sigma(X_train, q)
-        encoder = build_encoder(input_shape=(X_train.shape[-1],), units=128, n_components=2)
-        decoder = build_decoder(output_shape=(X_train.shape[-1],), units=128, n_components=2)
-        autoencoder = DeepDiffusionAE(encoder, decoder)
-        autoencoder.compile(X_train, sigma=sigma, steps=steps, kernel=kernel, alpha=alpha, diffusion_weight=diffusion_weight, optimizer='adam')
-        history = autoencoder.fit(X_train, epochs=300, validation_split=0.1, shuffle=True, batch_size=64, verbose=0)
-
-        X_train_red = autoencoder.encode(X_train)
-        X_test_red = autoencoder.encode(X_test)
-        X_train_rec = autoencoder.decode(X_train_red)
-        X_test_rec = autoencoder.decode(X_test_red)
-
-        X_train_rec = X_train_rec.numpy().reshape((X_train_rec.shape[0], *img_shape))
-        X_test_rec = X_test_rec.numpy().reshape((X_test_rec.shape[0], *img_shape))
-
-        plot_projection(X_train_red, y_train, title, path.join(root, experiment, 'train_red'))
-        plot_original(
-            X_train_rec, y_train, title,
-            path.join(root, experiment, 'train_rec'),
-            images_per_class=2, grid_shape=(3, 4)
-        )
-        plot_projection(X_test_red, y_test, title, path.join(root, experiment, 'test_red'))
-        plot_original(
-            X_test_rec, y_test, title,
-            path.join(root, experiment, 'test_rec'),
-            images_per_class=2, grid_shape=(3, 4)
-        )
-        plot_interpolations(
-            X_test_red, y_test, title,
-            decoder,
-            path.join(root, experiment, 'test_interp'),
-            img_shape,
-            class_pairs = [(i, i+1) for i in range(0, 6, 2)],
-            n_interpolations=4
-        )
-        plot_history(history, path.join(root, experiment, 'histories', title), log_scale=True)
-
-        compute_metrics(X_test, X_test_red, X_test_rec, y_test, title, path.join(root, experiment))        
+    compute_metrics(
+        X_train,
+        y_train,
+        X_train_red,
+        X_train_rec,
+        X_test,
+        y_test,
+        X_test_red,
+        X_test_rec,
+        time_in_sample,
+        time_out_of_sample,
+        title,
+        output_dir=path.join(root, title)
+    )
--- experiments/mnist/experiment_DDM.py
+++ experiments/mnist/experiment_DDM.py
@@ -1,4 +1,6 @@
+import time
 import numpy as np
+import tensorflow as tf
 from os import path
 
 from DiffusionLoss import DiffusionLoss
@@ -6,16 +8,12 @@ from aux_functions import get_sigma
 from experiments.mnist.plot_results import plot_original, plot_projection, plot_interpolations, plot_history
 from experiments.mnist.load_data import get_datasets
 from experiments.mnist.metrics import compute_metrics
-from experiments.utils import build_encoder, build_decoder
+from experiments.utils import build_conv_encoder, build_conv_decoder
 
 
+seed = 123
+tf.random.set_seed(seed)
 root = 'experiments/mnist/results/DDM'
-
-q_vals = [0.005, 0.005, 0.005, 0.005]
-steps_vals = [100, 100, 100, 100]
-alpha_vals = [1, 1, 1, 1]
-kernel = 'rbf'
-
 titles = [
     'Few samples without noise',
     'Many samples without noise',
@@ -23,67 +21,99 @@ titles = [
     'Many samples with noise'
 ]
 
-datasets_train, datasets_test = get_datasets(npoints=10000, test_size=0.1, seed=123, noise=0.25)
+datasets_train, datasets_test = get_datasets(npoints=1000, test_size=0.1, seed=seed, noise=0.25)
 
-for (X, y), title in zip(datasets_train, titles):
-    plot_original(
-        X, y, title, path.join(root, 'train_orig'), images_per_class=2, grid_shape=(3, 4)
-    )
-
-for (X, y), title in zip(datasets_test, titles):
-    plot_original(
-        X, y, title, path.join(root, 'test_orig'), images_per_class=2, grid_shape=(3, 4)
-    )
+q_vals = [5e-3, 5e-3, 2.5e-3, 2.5e-3]
+steps_vals = [100, 100, 100, 100]
+alpha_vals = [1, 1, 1, 1]
+kernel = 'rbf'
 
 for i in range(len(titles)):
     q, steps, alpha = q_vals[i], steps_vals[i], alpha_vals[i]
+    experiment = f'percentile_{q}-steps_{steps}-alpha_{alpha}'
     X_train, y_train = datasets_train[i]
     X_test, y_test = datasets_test[i]
+    # Añadimos la dimensión de canal
+    X_train = np.expand_dims(X_train, axis=-1)
+    X_test = np.expand_dims(X_test, axis=-1)
     title = titles[i]
-
-    img_shape = X_train.shape[1:]
-    X_train = X_train.reshape((X_train.shape[0], -1))
-    X_test = X_test.reshape((X_test.shape[0], -1))
+    sigma = get_sigma(X_train.reshape((X_train.shape[0], -1)), q)
     
-    sigma = get_sigma(X_train, q)
-    encoder = build_encoder(input_shape=(X_train.shape[-1],), units=128, n_components=2)
-    loss = DiffusionLoss(X_train, sigma=sigma, steps=steps, kernel=kernel, alpha=alpha)
+    print(experiment, '-', title)  
+    encoder = build_conv_encoder(input_shape=X_train.shape[1:], filters=8, n_components=2, zero_padding=(2, 2))
+    tic = time.perf_counter()
+    loss = DiffusionLoss(X_train.reshape((X_train.shape[0], -1)), sigma=sigma, steps=steps, kernel=kernel, alpha=alpha)
     encoder.compile(optimizer='adam', loss=loss)
     indices = np.array(list(range(len(X_train))))
-    hist_enc = encoder.fit(X_train, indices, epochs=300, validation_split=0.1, shuffle=True, batch_size=64, verbose=0)
+    hist_enc = encoder.fit(X_train, indices, epochs=10, validation_split=0.1, shuffle=True, batch_size=64, verbose=0)
     X_train_red = encoder(X_train)
+    tac = time.perf_counter()
     X_test_red = encoder(X_test)
+    toc = time.perf_counter()
 
-    decoder = build_decoder(output_shape=(X_train.shape[-1],), units=128, n_components=2)
+    decoder = build_conv_decoder(output_shape=X_train.shape[1:], filters=8, n_components=2, cropping=(2, 2))
     decoder.compile(optimizer='adam', loss='mse')
-    hist_dec = decoder.fit(X_train_red, X_train, epochs=300, validation_split=0.1, shuffle=True, batch_size=64, verbose=0)
-    X_train_rec = decoder(X_train_red)
-    X_test_rec = decoder(X_test_red)
-    
-    X_train_rec = X_train_rec.numpy().reshape((X_train_rec.shape[0], *img_shape))
-    X_test_rec = X_test_rec.numpy().reshape((X_test_rec.shape[0], *img_shape))
+    hist_dec = decoder.fit(X_train_red, X_train, epochs=10, validation_split=0.1, shuffle=True, batch_size=64, verbose=0)
+    X_train_rec = decoder(X_train_red).numpy()
+    X_test_rec = decoder(X_test_red).numpy()
 
-    plot_projection(X_train_red, y_train, title, path.join(root, 'train_red'))
+    time_in_sample = tac - tic
+    time_out_of_sample = toc - tac
+    
     plot_original(
-        X_train_rec, y_train, title,
-        path.join(root, 'train_rec'),
+        X_train, y_train,
+        path.join(root, title), 'train_orig',
         images_per_class=2, grid_shape=(3, 4)
     )
-    plot_projection(X_test_red, y_test, title, path.join(root, 'test_red'))
     plot_original(
-        X_test_rec, y_test, title,
-        path.join(root, 'test_rec'),
+        X_test, y_test,
+        path.join(root, title), 'test_orig',
+        images_per_class=2, grid_shape=(3, 4)
+    )
+    plot_projection(X_train_red, y_train, path.join(root, title), 'train_red',)
+    plot_original(
+        X_train_rec, y_train,
+        path.join(root, title), 'train_rec',
+        images_per_class=2, grid_shape=(3, 4)
+    )
+    plot_projection(X_test_red, y_test, path.join(root, title), 'test_red',)
+    plot_original(
+        X_test_rec, y_test,
+        path.join(root, title), 'test_rec',
         images_per_class=2, grid_shape=(3, 4)
     )
     plot_interpolations(
-        X_test_red, y_test, title,
+        X_train_red,
+        y_train,
         decoder,
-        path.join(root, 'test_interp'),
-        img_shape,
-        class_pairs = [(i, i+1) for i in range(0, 6, 2)],
-        n_interpolations=4
+        path.join(root, title),
+        'train_interp',
+        class_pairs = [(i, i+1) for i in range(0, 5)],
+        n_interpolations=6
     )
-    plot_history(hist_enc, path.join(root, 'histories', 'encoder', title), log_scale=True)
-    plot_history(hist_dec, path.join(root, 'histories', 'decoder', title), log_scale=True)
+    plot_interpolations(
+        X_test_red,
+        y_test,
+        decoder,
+        path.join(root, title),
+        'test_interp',
+        class_pairs = [(i, i+1) for i in range(0, 5)],
+        n_interpolations=6
+    )
+    plot_history(hist_enc, path.join(root, 'histories', title, experiment, 'encoder'), log_scale=True)
+    plot_history(hist_dec, path.join(root, 'histories', title, experiment, 'decoder'), log_scale=True)
 
-    compute_metrics(X_test, X_test_red, X_test_rec, y_test, title, root)        
+    compute_metrics(
+        X_train,
+        y_train,
+        X_train_red,
+        X_train_rec,
+        X_test,
+        y_test,
+        X_test_red,
+        X_test_rec,
+        time_in_sample,
+        time_out_of_sample,
+        title,
+        output_dir=path.join(root, title)
+    )
